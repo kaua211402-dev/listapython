5557bcba6a444e5c04c328dfb529d57ed0eeabc1
kaua211402-dev <kaua211402@gmail.com>
2026-05-04 07:52:05 -0300

    Update seletor.py

--- seletor.py
+++ seletor.py
@@ -1,1 +1,8 @@
-
+#seletor de caraceres--
+letra = input("digite uma letra").upper()
+if letra == 'A':
+print("voce selecionou a opcão A")
+elif letra == 'b':
+print("voce selecionou a opcão b")
+else:
+print("essa opcão não esta disponivel")
